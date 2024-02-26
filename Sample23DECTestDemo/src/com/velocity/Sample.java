872a5938cfda93bee4f0538e0b2ee54ac53d46b6
Jeevan <javaindustryexperience@gmail.com>
2024-02-26 08:19:40 +0530

    arrayList code added

---
+++ Sample23DECTestDemo/src/com/velocity/Sample.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(40);
+		al.add(70);
+		System.out.println(al);
+	}
+}
